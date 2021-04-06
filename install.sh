5cd6ff3bf42be5136d806c05698522c4987801e6
mike213112 <mikeramirez-14-31@hotmail.com>
2021-04-06 15:11:44 -0600

    second commit

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IPADDRESS() {
-	ADDIP="ip a add $1 dev $2 brd +"
-	echo `$ADDIP`
-}
+#IPADDRESS() {
+#	ADDIP="ip a add $1 dev $2 brd +"
+#	echo `$ADDIP`
+#}
 
 
 INSTALLSQLSERVER() {
@@ -39,7 +39,7 @@ INSTALLSQLSERVER() {
 }
 
 INSTALLORACLE() {
-		
+	echo "hola"		
 }
 
 #IPADDRESS="ip a add '$1' "
@@ -49,9 +49,71 @@ saludar() {
 	echo $hola
 }
 
-  
 
-echo "Ingresa tu nombre"
-read nombre
+InstalarSqlServer() {
+	python3 -c "
 
-saludar $nombre
+import os
+
+class InstalarSqlServer:
+	def __init__(self,file = 'sqlserver.txt'):
+		self.file = file
+
+	def install(self):
+		file = open(self.file,'r')
+		data = file.read()
+
+		for i in data.split(';'):
+			os.system(i)
+
+installer = InstalarSqlServer()
+installer.install()
+
+	"
+}
+
+
+#echo " _____   ____  _____    ______    _________        _         _____           _        ______       ___     _______    "
+#echo "|_   _| |_   \|_   _| .' ____ \  |  _   _  |      / \       |_   _|         / \      |_   _ ` .  .'   `.  |_   __ \   "
+#echo "  | |     |   \ | |   | (___ \_| |_/ | | \_|     / _ \        | |          / _ \       | | `. \ /  .-.  \   | |__) |  " 
+#echo "  | |     | |\ \| |    _.____`.      | |        / ___ \       | |   _     / ___ \      | |  | | | |   | |   |  __ /   " 
+#echo " _| |_   _| |_\   |_  | \____) |    _| |_     _/ /   \ \_    _| |__/ |  _/ /   \ \_   _| |_.' / \  `-'  /  _| |  \ \_ " 
+#echo "|_____| |_____|\____|  \______.'   |_____|   |____| |____|  |________| |____| |____| |______.'   `.___.'  |____| |___|" 
+
+
+#echo "I)iiii N)n   nn  S)ssss  T)tttttt   A)aa   L)         A)aa   D)dddd    O)oooo  R)rrrrr  "
+#echo "  I)   N)nn  nn S)    ss    T)     A)  aa  L)        A)  aa  D)   dd  O)    oo R)    rr "
+#echo "  I)   N) nn nn  S)ss       T)    A)    aa L)       A)    aa D)    dd O)    oo R)  rrr  "
+#echo "  I)   N)  nnnn      S)     T)    A)aaaaaa L)       A)aaaaaa D)    dd O)    oo R) rr    "
+#echo "  I)   N)   nnn S)    ss    T)    A)    aa L)       A)    aa D)    dd O)    oo R)   rr  "
+#echo "I)iiii N)    nn  S)ssss     T)    A)    aa L)llllll A)    aa D)ddddd   O)oooo  R)    rr "
+
+echo "	  ___   _   _   ____    _____      _      _          _      ____     ___    ____   "
+echo "	 |_ _| | \ | | / ___|  |_   _|    / \    | |        / \    |  _ \   / _ \  |  _ \  "
+echo "	  | |  |  \| | \___ \    | |     / _ \   | |       / _ \   | | | | | | | | | |_) | "
+echo "	  | |  | |\  |  ___) |   | |    / ___ \  | |___   / ___ \  | |_| | | |_| | |  _ <  "
+echo "	 |___| |_| \_| |____/    |_|   /_/   \_\ |_____| /_/   \_\ |____/   \___/  |_| \_\ "
+echo "                                                                                     "
+echo "                                                                                     "
+echo "[0] Salir                                                                            "
+echo "[1] Instalar SQLServer                                                               "
+echo "[2] Instalar Oracle                                                                  "
+echo "                                                                                     "
+echo "Elije una opcion                                                                     "
+read opcion
+
+if [ $opcion = 0 ]; then
+	exit 0
+elif [ $opcion = 1 ]; then
+	InstalarSqlServer
+elif [ $opcion = 2 ]; then
+	echo "hola"
+fi
+
+
+#InstalarSqlServer
+
+#echo "Ingresa tu nombre"
+#read nombre
+
+#saludar $nombre
